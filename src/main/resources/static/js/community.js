2a22895517052a6fea113c2bc7aaa0df6fa478c0
朱培挺 <1055363759@qq.com>
2022-10-18 17:11:31 +0800

    添加回复功能的前端界面，并基于ajax实现了异步post请求回复API将回复内容发送到API并接收API的响应信息，前端做出初步的响应

---
+++ src/main/resources/static/js/community.js
@@ -0,0 +1,23 @@
+function post(){
+    var questionId = $("#question_id").val();
+    var commentContent = $("#comment_content").val();
+    $.ajax({
+        type:"POST",
+        url:"/comment",
+        contentType:"application/json",
+        data:JSON.stringify({
+            "parentId":questionId,
+            "content":commentContent,
+            "commentType":1
+        }),
+        success:function (response){
+            if(response.code == 200){
+                $("#comment_section").hide();
+            }else{
+                alert(response.message);
+            }
+            console.log(response);
+        },
+        dataType:"json"
+    })
+}
